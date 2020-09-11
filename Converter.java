035eccc2b420f0450530f80b2bf9eec28d2a35eb
jviteMx <64880840+jviteMx@users.noreply.github.com>
2020-09-11 13:17:31 -0500

    Update Converter.java
    added 2 lines for the main method

--- Converter.java
+++ Converter.java
@@ -2,24 +2,27 @@ public class Converter {
 	//Your names go here:
 
 	/*
-	 * @Author: Name of the first student
-	 * Sarim Tariq
+	 * @Author: yobbahim Javier Israel
+	 * Name of the second student
 	 * Name of the third student
 	 */
 	
-	private double celsiusToFahrenheit(double C) {
+	private static double celsiusToFahrenheit(double C) {
 		// TODO: The third student will implement this method
 		return 0;
 	}
 	
-	private double fahrenheitToCelsius(double F) {
+	private static double fahrenheitToCelsius(double F) {
 		// TODO: The second student will implement this method 
-		double C = (F - 32.0) * (5.0/9.0);
-		return C;
+		return 0;
 	}
 	public static void main(String[] args) {
 		//TODO:The first student will implement this method.
 		// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+		System.out.println(celsiusToFahrenheit(180));
+		
+		
 		// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.}}
+		System.out.println(fahrenheitToCelsius(250));
 	}
 }
